5bc1059058d60efc2d44f52ca16350b307ff6c2f
Oleksandr Slobodian <80710817+oleksandr-zeero@users.noreply.github.com>
2026-05-20 23:08:38 +0300

    Add files via upload

---
+++ lab3/ServerLab3/ClientLab3/ClientLab3.cpp
@@ -0,0 +1,59 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <string>
+#define MAILSLOT_PATH L"\\\\.\\mailslot\\DiscoveryV11"
+#define PIPE_PATH L"\\\\.\\pipe\\ForumV11"
+
+DWORD WINAPI Receiver(LPVOID lpParam) {//потік який ловить повідомлення від сервера
+    HANDLE hPipe = (HANDLE)lpParam;
+    char buf[1024];
+    DWORD rd, avail;
+
+    while (TRUE) {
+        if (PeekNamedPipe(hPipe, NULL, 0, NULL, &avail, NULL) && avail > 0) {// пере-віряємо наявність даних кожні 10 мс
+            if (ReadFile(hPipe, buf, sizeof(buf), &rd, NULL) && rd > 0) {
+                printf("\r[Forum]: %s\n> ", buf);
+                fflush(stdout);
+            }
+        }
+        Sleep(10);
+    }
+    return 0;
+}
+
+int main() {
+    SetConsoleOutputCP(1251); SetConsoleCP(1251);
+    printf("=== CLIENT  ===\n");
+    HANDLE hFile = INVALID_HANDLE_VALUE;
+    while (hFile == INVALID_HANDLE_VALUE) {//намагаємось підключитись до поштової скриньки сервера
+        hFile = CreateFile(MAILSLOT_PATH, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (hFile == INVALID_HANDLE_VALUE) Sleep(500);
+    }
+    DWORD wr; WriteFile(hFile, "PING", 5, &wr, NULL); CloseHandle(hFile);
+    HANDLE hPipe = CreateFile(PIPE_PATH, GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);//підключення до pipe
+    if (hPipe == INVALID_HANDLE_VALUE) return 1;
+    char nick[50];
+    printf("Set nickname: ");
+    fgets(nick, 50, stdin);
+    nick[strcspn(nick, "\n")] = 0;
+    CreateThread(NULL, 0, Receiver, (LPVOID)hPipe, 0, NULL);// запускаємо потік читан-ня
+    Sleep(100);
+
+    std::string joinMsg = "User [" + std::string(nick) + "] connected!";// handshake
+    WriteFile(hPipe, joinMsg.c_str(), (DWORD)joinMsg.length() + 1, &wr, NULL);
+    FlushFileBuffers(hPipe);
+
+    char msg[400];//відправка повідомлень
+    while (TRUE) {
+        printf("> ");
+        if (fgets(msg, 400, stdin)) {
+            msg[strcspn(msg, "\n")] = 0;
+            if (strlen(msg) > 0) {
+                std::string fullMsg = std::string(nick) + ": " + msg;
+                WriteFile(hPipe, fullMsg.c_str(), (DWORD)fullMsg.length() + 1, &wr, NULL);
+                FlushFileBuffers(hPipe);
+            }
+        }
+    }
+    return 0;
+}
---
+++ lab3/ServerLab3/ServerLab3/ServerLab3.cpp
@@ -0,0 +1,76 @@
+﻿#include <windows.h>
+#include <stdio.h>
+#include <vector>
+#include <string>
+#define PIPE_NAME L"\\\\.\\pipe\\ForumV11"
+#define MAILSLOT_NAME L"\\\\.\\mailslot\\DiscoveryV11"
+
+CRITICAL_SECTION cs;
+std::vector<HANDLE> hClients;//список активних підключень
+
+void Broadcast(const char* msg) {//розсилка повідомлень всім підключеним клієнтам
+    std::vector<HANDLE> targets;// копіюємо список клієнтів, щоб не тримати секцію заблокованою під час запису
+    EnterCriticalSection(&cs);//блокуємо доступ до списку клієнтів для інших потоків
+    targets = hClients;
+    LeaveCriticalSection(&cs);
+
+    for (HANDLE h : targets) {//проходимось циклом по всіх клієнтах і відправляємо кожному з них повідомлення
+        DWORD wr;
+        if (WriteFile(h, msg, (DWORD)strlen(msg) + 1, &wr, NULL)) {
+            FlushFileBuffers(h);
+        }
+    }
+}
+
+DWORD WINAPI ClientHandler(LPVOID lpParam) {//потік для обробки кожного клієнта
+    HANDLE hPipe = (HANDLE)lpParam;
+    char buf[1024];
+    DWORD rd, avail;
+
+    while (TRUE) {
+        if (PeekNamedPipe(hPipe, NULL, 0, NULL, &avail, NULL) && avail > 0) {//перевірка чи є нові дані
+            if (ReadFile(hPipe, buf, sizeof(buf), &rd, NULL) && rd > 0) {//якщо є - читаємо
+                printf("[SERVER LOG]: %s\n", buf);
+                Broadcast(buf);
+            }
+        }
+        else if (GetLastError() == ERROR_BROKEN_PIPE) {// клієнт закрив програму
+            break;
+        }
+        Sleep(10);
+    }
+    EnterCriticalSection(&cs);//відключення клієнта
+    for (auto it = hClients.begin(); it != hClients.end(); ++it) {
+        if (*it == hPipe) { hClients.erase(it); break; }
+    }
+    LeaveCriticalSection(&cs);//блокуємо список
+    CloseHandle(hPipe);
+    return 0;
+}
+DWORD WINAPI Discovery(LPVOID lpParam) {//потік який слухає mailslot на наявність ping-запитів
+    HANDLE hSlot = CreateMailslot(MAILSLOT_NAME, 0, MAILSLOT_WAIT_FOREVER, NULL);//створюємо mailslot
+    char b[128]; DWORD r;
+    while (ReadFile(hSlot, b, sizeof(b), &r, NULL)) printf("[DISCOVERY]: Ping!\n");//чекаємо на повідомлення
+    return 0;
+}
+
+int main() {
+    SetConsoleOutputCP(1251); SetConsoleCP(1251);
+    InitializeCriticalSection(&cs);
+    printf("=== SERVER ===\n");
+    CreateThread(NULL, 0, Discovery, NULL, 0, NULL);
+    while (TRUE) {
+        HANDLE hPipe = CreateNamedPipe(PIPE_NAME, PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES, 2048, 2048, 0, NULL);
+        if (ConnectNamedPipe(hPipe, NULL) || GetLastError() == ERROR_PIPE_CONNECTED) {//додаємо нового клієнта до списку
+            printf("[CONNECT]: Client connected.\n");
+            EnterCriticalSection(&cs);
+            hClients.push_back(hPipe);
+            LeaveCriticalSection(&cs);
+            CreateThread(NULL, 0, ClientHandler, (LPVOID)hPipe, 0, NULL);//створюємо потік для робти з ним
+        }
+    }
+    return 0;
+}
+
